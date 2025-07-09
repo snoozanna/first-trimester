07bef26405073006514f33222f875935f3126984
snoozanna <hurstsuzanna@gmail.com>
2025-07-09 11:01:10 +0100

    new form link

--- src/pages/participate.js
+++ src/pages/participate.js
@@ -111,7 +111,7 @@ const DonatePage = ({ location }) => {
                   <span className="bullet-wrapper">
                     <img src={spermAlone} alt="" />
                   </span>
-                  Fill out this quick <a href='https://airtable.com/appa22sJWd9yij9JZ/shrSLnXciscNMf3To' rel="noreferrer" target="_blank">form</a>.
+                  Fill out this quick <a href='https://airtable.com/appa22sJWd9yij9JZ/pagwcnj2OR6BW4SeC/form' rel="noreferrer" target="_blank">form</a>.
                 </li>
                 <li data-sal="fade" data-sal-delay="500" data-sal-easing="ease">
                   <span className="bullet-wrapper">
